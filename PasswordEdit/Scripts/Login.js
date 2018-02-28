d75783fc141e8ac65ca3a75d9d07ca69268e4a8b
KomeijiLogi <zclxuan2012@163.com>
2018-02-28 10:44:32 +0800

    调整后的密码修改

---
+++ PasswordEdit/Scripts/Login.js
@@ -0,0 +1,35 @@
+﻿function clickEvent() {
+    
+    $("#loadingAnimate").show();
+    $("#confirmPw").attr('disabled', 'disabled');
+    mui.toast('已点击密码修改，请稍后');
+     
+    //$("#confirmPw").attr('disabled', 'disabled');           //调整提交按钮为禁用 
+    $.ajax({
+        method: 'post',
+        url: '/Home/Index',
+        data: $('form').serialize()
+
+    }).done(function (data) {
+        $("#loadingAnimate").hide();
+        mui.alert(data);
+        $('input[type="password"]').val('');
+        $("#confirmPw").removeAttr('disabled');
+    }).fail(function (e) {
+        $("#confirmPw").removeAttr('disabled');
+    });
+   
+}
+
+
+
+mui.init();
+
+mui.ready(function () {
+    //clickEvent();
+
+});
+
+    
+
+
---
+++ PasswordEdit/obj/Release/Package/PackageTmp/Scripts/Login.js
@@ -0,0 +1,32 @@
+﻿function clickEvent() {
+    
+    $("#loadingAnimate").show();
+    mui.toast('已点击密码修改，请稍后');
+     
+    //$("#confirmPw").attr('disabled', 'disabled');           //调整提交按钮为禁用 
+    $.ajax({
+        method: 'post',
+        url: '/Home/Index',
+        data: $('form').serialize()
+
+    }).done(function (data) {
+        $("#loadingAnimate").hide();
+        mui.alert(data);
+    }).fail(function (e) {
+
+    });
+   
+}
+
+
+
+mui.init();
+
+mui.ready(function () {
+    //clickEvent();
+
+});
+
+    
+
+
